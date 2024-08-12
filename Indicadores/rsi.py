400c449306a30fe1f35dba5bbffff3f951996a59
Denner Santos <basilio949@gmail.com>
2024-08-12 12:19:25 -0400

    Atualizacao RSI

--- Indicadores/rsi.py
+++ Indicadores/rsi.py
@@ -1,8 +1,6 @@
 from .indicador import Indicador
-import pandas as pd
 import matplotlib.pyplot as plt
 import math
-from matplotlib.dates import DateFormatter
 import numpy as np
 
 class RSI(Indicador):
@@ -17,20 +15,13 @@ class RSI(Indicador):
         if len(self.df) > 0 and linha['date'] == self.df['date'].iloc[-1]:
             return self.df.at[len(self.df) - 1, 'decisao']
         self.set_linha_df(linha)
-        self.calcular_diferenca_por_linha()
-        self.calcular_ganho_perda_por_linha()
-        self.calcular_media_ganho_perda_por_linha(self.periodo)
-        self.calcular_rsi_por_linha()
-        self.tomar_decisao_RSI_por_linha()
+        self.calcular_diferenca()
+        self.calcular_ganho_perda()
+        self.calcular_media_ganho_perda(self.periodo)
+        self.calcular_rsi()
+        self.tomar_decisao_RSI()
 
         return self.df.at[len(self.df) - 1, 'decisao']
-
-        # sinais = []
-        # for rsi_valor in self.df['rsi']:
-        #     decisao = self.analisar_decisao_RSI(rsi_valor)
-        #     sinais.append(decisao)
-
-        #return sinais[-1]
 
     def get_topo(self):
         return self.topo
@@ -44,57 +35,6 @@ class RSI(Indicador):
     def set_baixa(self, valor):
         self.baixa = valor
         
-    def calcular_diferenca(self):
-        df = self.df.copy()
-        df['diferenca'] = df['close'].diff()
-        return df
-
-    def calcular_ganho_perda(self):
-        self.df['ganho'] = 0
-        self.df['perda'] = 0
-
-        for i in range(1, len(self.df)):
-            diferenca = self.df.at[i, 'diferenca']
-            if diferenca > 0:
-                self.df.at[i, 'ganho'] = diferenca
-            else:
-                self.df.at[i, 'perda'] = abs(diferenca)
-
-        return self.df
-
-    def calcular_media_ganho_perda(self, periodo):
-        self.df['media_ganho'] = 0
-        self.df['media_perda'] = 0
-
-        for i in range(1, len(self.df)):
-            if i < periodo:
-                # Calcula a média simples das primeiras 'i' linhas
-                media_ganho = self.df['ganho'][:i].mean()
-                media_perda = self.df['perda'][:i].mean()
-            else:
-                # Calcula a média ponderada a partir do período definido
-                media_ganho = ((self.df['media_ganho'][i - 1] * (periodo - 1)) + self.df['ganho'][i]) / periodo
-                media_perda = ((self.df['media_perda'][i - 1] * (periodo - 1)) + self.df['perda'][i]) / periodo
-
-            self.df.at[i, 'media_ganho'] = media_ganho
-            self.df.at[i, 'media_perda'] = media_perda
-
-        return self.df
-
-    def calcular_rsi(self):
-        for i in range(len(self.df)):
-            media_ganho = self.df.at[i, 'media_ganho']
-            media_perda = self.df.at[i, 'media_perda']
-
-            if media_perda == 0:
-                rs = 0
-            else:
-                rs = media_ganho / media_perda
-
-            rsi = 100 - (100 / (1 + rs))
-            self.df.at[i, 'rsi'] = rsi
-
-        return self.df
 
     def analisar_decisao_RSI(self, rsi_valor):
         if rsi_valor == 0:
@@ -106,32 +46,8 @@ class RSI(Indicador):
         else:
             return "Manter"
         
-    def plotar_grafico(self):
-        self.df['rsi'] = self.df['rsi'].astype(float)
-        
-        plt.figure(figsize=(10, 5))
-        
-        # Plot RSI
-        plt.plot(self.df['date'], self.df['rsi'], label='RSI', color='blue')
-        
-        # Plot Topo and Baixa as lines
-        plt.plot(self.df['date'], self.df['topo'], label='Topo', color='red', linestyle='--')
-        plt.plot(self.df['date'], self.df['baixa'], label='Baixa', color='green', linestyle='--')
-        
-        plt.title('Gráfico de RSI')
-        plt.xlabel('Data')
-        plt.ylabel('RSI')
-        
-        espacamento = math.ceil(len(self.df['date']) / 10)
-        plt.xticks(self.df['date'][::espacamento], rotation=20)
-        
-        plt.legend()
-        plt.show()
-        
-        
-        
-        #Funcoes por linha
-    def calcular_diferenca_por_linha(self):
+
+    def calcular_diferenca(self):
         index_nova_linha = len(self.df) - 1
         close_atual = self.df['close'].iloc[index_nova_linha]
         if index_nova_linha < 1:
@@ -141,7 +57,7 @@ class RSI(Indicador):
         self.df.loc[index_nova_linha, 'diferenca'] = close_atual - close_anterior
         return self.df
 
-    def calcular_ganho_perda_por_linha(self):
+    def calcular_ganho_perda(self):
         index_nova_linha = len(self.df) - 1
         self.df.loc[index_nova_linha, 'ganho'] = 0
         self.df.loc[index_nova_linha, 'perda'] = 0
@@ -155,7 +71,7 @@ class RSI(Indicador):
 
         return self.df
 
-    def calcular_media_ganho_perda_por_linha(self, periodo):
+    def calcular_media_ganho_perda(self, periodo):
         index_nova_linha = len(self.df) - 1
 
         if len(self.df) < periodo:
@@ -165,11 +81,10 @@ class RSI(Indicador):
             self.df.at[index_nova_linha, 'media_ganho'] = media_ganho
             self.df.at[index_nova_linha, 'media_perda'] = media_perda
         else:
-            # Se não, calcula as médias móveis usando a última linha adicionada
             media_ganho_anterior = self.df.at[index_nova_linha - 1, 'media_ganho']
             media_perda_anterior = self.df.at[index_nova_linha - 1, 'media_perda']
 
-            # Atualiza as médias móveis com base na última linha adicionada
+            # Atualiza as médias móveis com a última linha adicionada
             media_ganho_nova = ((media_ganho_anterior * (periodo - 1)) + self.df.at[index_nova_linha, 'ganho']) / periodo
             media_perda_nova = ((media_perda_anterior * (periodo - 1)) + self.df.at[index_nova_linha, 'perda']) / periodo
 
@@ -178,26 +93,21 @@ class RSI(Indicador):
 
         return self.df
 
-    def calcular_rsi_por_linha(self):
+    def calcular_rsi(self):
         index_nova_linha = len(self.df) - 1
 
         media_ganho = self.df.at[index_nova_linha, 'media_ganho']
         media_perda = self.df.at[index_nova_linha, 'media_perda']
 
-        # Calcular o RSI para a última linha
         if media_perda == 0:
             rs = 0
         else:
             rs = media_ganho / media_perda
 
         rsi = 100 - (100 / (1 + rs))
-
         self.df.at[index_nova_linha, 'rsi'] = rsi
-
         return self.df
     
-    
-
     def atualizar_niveis_rsi(self, tamanho_janela=20, intervalo_recalculo=10):
         if len(self.df) >= tamanho_janela and len(self.df) % intervalo_recalculo == 0:
             valores_rsi = self.df['rsi'][-tamanho_janela:]
@@ -210,12 +120,32 @@ class RSI(Indicador):
             self.set_baixa(np.floor(baixa))
 
     
-    def tomar_decisao_RSI_por_linha(self):
+    def tomar_decisao_RSI(self):
         index_nova_linha = len(self.df) - 1
         rsi_valor = self.df.at[index_nova_linha, 'rsi']
         self.df.at[index_nova_linha, 'decisao'] = self.analisar_decisao_RSI(rsi_valor)
         self.df.at[index_nova_linha, 'topo'] = self.topo
         self.df.at[index_nova_linha, 'baixa'] = self.baixa
         #self.atualizar_niveis_rsi(20, 10)
+        
+    def plotar_grafico(self):
+        self.df['rsi'] = self.df['rsi'].astype(float)
+        
+        plt.figure(figsize=(10, 5))
+        
+        plt.plot(self.df['date'], self.df['rsi'], label='RSI', color='blue')
+        
+        plt.plot(self.df['date'], self.df['topo'], label='Topo', color='red', linestyle='--')
+        plt.plot(self.df['date'], self.df['baixa'], label='Baixa', color='green', linestyle='--')
+        
+        plt.title('Gráfico de RSI')
+        plt.xlabel('Data')
+        plt.ylabel('RSI')
+        
+        espacamento = math.ceil(len(self.df['date']) / 10)
+        plt.xticks(self.df['date'][::espacamento], rotation=20)
+        
+        plt.legend()
+        plt.show()
             
 
