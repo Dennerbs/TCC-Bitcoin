537cb687cc4b9e6bd2efc723d2f2883ea41e93cb
Denner Santos <basilio949@gmail.com>
2024-08-17 18:08:29 -0400

    melhoria construcao logs

--- Operacao/operacao.py
+++ Operacao/operacao.py
@@ -1,5 +1,5 @@
 from API.binance_api import get_dados_criptomoeda, tempo_intervalo, get_dados_bitcoin_websocket, get_valor_minimo_operacao, comprar_ativo, vender_ativo, tratar_ordem
-from Utils.utils import calibrar_df_indicadores
+from Utils.utils import calibrar_df_indicadores, formatar_log_compra_venda, formatar_log_indicador
 import logging
 import asyncio
 
@@ -43,15 +43,7 @@ async def trade(indicadores, valor_total, intervalo='1h', simbolo='BTCBRL'):
                         indicador.set_quantidade_vendas(valor_operacao)
                         indicador.set_somatorio_taxas(dados_ordem['taxa_em_real'])
                         indicador.set_quantidade_bitcoin(0)
-                        log_venda = {
-                            "ciclo": contador,
-                            "indicador": indicador.nome_indicador,
-                            "sinal": sinal,
-                            "quantidade_ordem": quantidade_bitcoin,
-                            "quantidade_efetivada": dados_ordem['quantidade_ativo'],
-                            "valor_bitcoin": dados_ordem['valor_ativo'],
-                            "valor_venda": valor_operacao,
-                        }
+                        log_venda = formatar_log_compra_venda(contador, indicador.nome_indicador, sinal, quantidade_bitcoin, dados_ordem, valor_operacao)
                         logging.info(f'log_venda: {log_venda}')
                     except Exception as e:
                         logging.error(f'Erro ao vender ativo ou tratar ordem: {e}')
@@ -68,48 +60,22 @@ async def trade(indicadores, valor_total, intervalo='1h', simbolo='BTCBRL'):
                         quantidade_a_comprar =  valor_disponivel / linha['close']
                         ordem = comprar_ativo(quantidade_a_comprar)
                         dados_ordem = tratar_ordem(ordem)
-                    
-                        quantidade_comprada = dados_ordem['quantidade_ativo']
                         valor_operacao = dados_ordem['valor_operacao']
-                        indicador.set_quantidade_bitcoin(quantidade_comprada)
+                        indicador.set_quantidade_bitcoin(dados_ordem['quantidade_ativo'])
                         indicador.set_valor_ultima_compra(valor_operacao)
                         indicador.set_valor_disponivel(0)
                         indicador.set_somatorio_taxas(dados_ordem['taxa_em_real'])
-                        log_compra = {
-                            "ciclo": contador,
-                            "indicador": indicador.nome_indicador,
-                            "sinal": sinal,
-                            "quantidade_ordem": quantidade_a_comprar,
-                            "quantidade_efetivada": quantidade_comprada,
-                            "valor_bitcoin": dados_ordem['valor_ativo'],
-                            "valor_compra": valor_operacao,
-                        }
+                        log_compra = formatar_log_compra_venda(contador, indicador.nome_indicador, sinal, quantidade_a_comprar, dados_ordem, valor_operacao)
                         logging.info(f'log_compra: {log_compra}')
                     except Exception as e:
                         logging.error(f'Erro ao comprar ativo ou tratar ordem: {e}')
                         continue
-            saldo_indicador = indicador.get_valor_disponivel() + indicador.get_quantidade_bitcoin() * linha['close']
-            taxas_indicador = indicador.get_somatorio_taxas_transacao()
-            ganhos_indicador = indicador.get_somatorio_ganhos()
-            perdas_indicador = indicador.get_somatorio_perdas()
-        
-            log_indicador = {
-                "nome_indicador" : indicador.nome_indicador,
-                "Saldo indicador": saldo_indicador,
-                "Lucro": indicador.calcular_lucro(),
-                "Lucro por operacao": indicador.calcular_lucro_por_operacao(),
-                "Somatorio ganhos": ganhos_indicador,
-                "Somatorio perdas": perdas_indicador,
-                "Ganhos por operacao": indicador.calcular_ganho_por_venda(),
-                "Perdas por operacao": indicador.calcular_perda_por_venda(),
-                "Somatorio Taxas de operacao": taxas_indicador,
-                "Taxas por operacao": indicador.calcular_taxa_por_operacao()
-            }
+            log_indicador = formatar_log_indicador(indicador, linha['close'])
             logging.info(f'log_indicador: {log_indicador}')
-            saldo_total += saldo_indicador    
-            taxas_total += taxas_indicador    
-            ganhos_total += ganhos_indicador    
-            perdas_total += perdas_indicador    
+            saldo_total += log_indicador['Saldo indicador']    
+            taxas_total += log_indicador['Somatorio Taxas de operacao']    
+            ganhos_total += log_indicador['Somatorio ganhos']    
+            perdas_total += log_indicador['Somatorio perdas']    
   
 
         log_saldo = {
