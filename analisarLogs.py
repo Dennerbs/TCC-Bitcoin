c0255c83377355dbf8d99fedfc21d4f34b4fdf4c
Denner Santos <basilio949@gmail.com>
2024-09-07 17:03:01 -0400

    Ajuste analisarLogs

--- analisarLogs.py
+++ analisarLogs.py
@@ -4,9 +4,13 @@ import re
 from Graficos.negociacoes import plotar_negociacoes, plotar_evolucao_dinheiro
 
 def ler_logs_arquivo(arquivo_log):
-    with open(arquivo_log, 'r', encoding='utf-8') as arquivo:
+    with open(arquivo_log, 'r') as arquivo:
         logs = arquivo.readlines()
     return logs
+
+def filtrar_logs(logs):
+    logs_filtrados = [linha for linha in logs if "- INFO - log_" in linha]
+    return logs_filtrados
 
 def extrair_dados_logs(logs):
     dados_logs = {
@@ -50,20 +54,26 @@ def extrair_dados_logs(logs):
 def extrair_dados_graficos(dados_logs):
     datas = []
     fechamento = []
-    sinais_compra = {"Volume": {"x": [], "y": []}, "RSI": {"x": [], "y": []}, "MACD": {"x": [], "y": []}}
-    sinais_venda = {"Volume": {"x": [], "y": []}, "RSI": {"x": [], "y": []}, "MACD": {"x": [], "y": []}}
+    sinais_compra = {}
+    sinais_venda = {}
 
     for log in dados_logs["log_saldo"]:
         datas.append(log['Data'])
         fechamento.append(log["valor_bitcoin"])
 
     for log in dados_logs["log_compra"]:
-        sinais_compra[log["indicador"]]["x"].append(log['Data'])
-        sinais_compra[log["indicador"]]["y"].append(log["valor_bitcoin"])
+        indicador = log["indicador"]
+        if indicador not in sinais_compra:
+            sinais_compra[indicador] = {"x": [], "y": []}
+        sinais_compra[indicador]["x"].append(log['Data'])
+        sinais_compra[indicador]["y"].append(log["valor_bitcoin"])
         
     for log in dados_logs["log_venda"]:
-        sinais_venda[log["indicador"]]["x"].append(log['Data'])
-        sinais_venda[log["indicador"]]["y"].append(log["valor_bitcoin"])
+        indicador = log["indicador"]
+        if indicador not in sinais_venda:
+            sinais_venda[indicador] = {"x": [], "y": []}
+        sinais_venda[indicador]["x"].append(log['Data'])
+        sinais_venda[indicador]["y"].append(log["valor_bitcoin"])
 
     valores = {
         "x": datas,
@@ -72,8 +82,10 @@ def extrair_dados_graficos(dados_logs):
 
     return datas, fechamento, sinais_compra, sinais_venda, valores
 
-logs = ler_logs_arquivo('trade_logs.log')
-dados_logs = extrair_dados_logs(logs)
+logs = ler_logs_arquivo('trade_logs_TESTE.log')
+logs_filtrados = filtrar_logs(logs)
+dados_logs = extrair_dados_logs(logs_filtrados)
 datas, fechamento, sinais_compra, sinais_venda, valores = extrair_dados_graficos(dados_logs)
 plotar_negociacoes(datas, fechamento, sinais_compra, sinais_venda)
-plotar_evolucao_dinheiro(valores, 1000, True, sinais_compra, pd.DataFrame({'date': datas, 'close': fechamento}))
+
+plotar_evolucao_dinheiro(valores, 59, True, sinais_compra, pd.DataFrame({'date': datas, 'close': fechamento}))
